ac67c6335a8904449e0cabcb42663abc0ad4e153
Dani Adkins <danirenaeadkins12@gmail.com>
2017-03-15 21:02:28 -0500

    project setup

---
+++ app/app.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var app = angular.module("Capstone", ['ngRoute'])
+    .constant('apiUrl', 'http://localhost:7000')
+
+angular.module("Capstone").config(
+    [
+        '$interpolateProvider',
+        '$httpProvider',
+        '$routeProvider',
+        '$locationProvider',
+        function($interpolateProvider, $httpProvider, $routeProvider, $locationProvider) {
+
+            $interpolateProvider.startSymbol('{{');
+            $interpolateProvider.endSymbol('}}');
+
+            $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+            $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+            $routeProvider
+                .when('/home', {
+                    controller: "HomeCtrl",
+                    templateUrl: 'partials/home.html'
+                })
+        }
+]);
